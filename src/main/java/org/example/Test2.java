a3f7673cd214ed205d35d7b00e3cf56158e3ad47
Bishwajeet Kumar <barnwalbishwa@gmail.com>
2025-01-27 21:41:55 +0000

    [Bish-123] commit the code (#1)

---
+++ src/main/java/org/example/Test2.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Test2 {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("Banana");
+        list.add("Apple");
+        list.add("Mango");
+
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
