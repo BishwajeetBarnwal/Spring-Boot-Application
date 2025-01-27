d18d92639f4e3df239948b572df61f9b46c340f2
Bishwajeet Kumar <barnwalbishwa@gmail.com>
2025-01-27 21:37:24 +0000

    [Bish-123] commit the code

---
+++ src/main/java/Sonu.java
@@ -0,0 +1,33 @@
+import java.util.stream.Collectors;
+
+public class Sonu {
+    //list of integers
+    //years 1998,1996, 2005, 2007
+    //print the list as per leap year
+    public static void main(String[] args) {
+        list.add("1998");
+        list.add("1996");
+        list.add("2000");
+        list.add("2008");
+
+        years = list.get();
+        list = stream.sorted().collect(Collectors.toList(years * 4));
+        count = list.size();
+        System.out.println("Leap year " + count);
+    }
+}
+
+//2nd max salary
+
+select max(sal) from emp where sal<=(select max(sal) from emp);
+
+select sal from emp where denc_rank (0,2) order by sal;
+
+//test the private method in unit test cases
+@test
+public void testPrivatemethod() throws Exception {
+    Method method = MyClass.class.getDeclared;
+    reflectMethod.setAccessible(true);
+}
+
+Design patterns, Design principles, Solid principle.
