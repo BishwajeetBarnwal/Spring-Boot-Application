137ef634672db211e866862367fb2762252ed80e
Bishwajeet Kumar <barnwalbishwa@gmail.com>
2025-02-08 18:59:32 +0000

    Updated the file.

--- src/main/java/Sonu.java
+++ src/main/java/Sonu.java
@@ -1,33 +1,37 @@
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Sonu {
     //list of integers
     //years 1998,1996, 2005, 2007
     //print the list as per leap year
     public static void main(String[] args) {
-        list.add("1998");
-        list.add("1996");
-        list.add("2000");
-        list.add("2008");
+    	List<Integer> years = Arrays.asList(1998, 1996, 2005, 2007);
 
-        years = list.get();
-        list = stream.sorted().collect(Collectors.toList(years * 4));
-        count = list.size();
-        System.out.println("Leap year " + count);
+        List<Integer> leapYears = years.stream()
+                .filter(Sonu::isLeapYear)
+                .collect(Collectors.toList());
+
+        System.out.println(leapYears); // Output: [1996]
     }
-}
+
+    public static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+    }}
 
 //2nd max salary
 
-select max(sal) from emp where sal<=(select max(sal) from emp);
-
-select sal from emp where denc_rank (0,2) order by sal;
-
-//test the private method in unit test cases
-@test
-public void testPrivatemethod() throws Exception {
-    Method method = MyClass.class.getDeclared;
-    reflectMethod.setAccessible(true);
-}
-
-Design patterns, Design principles, Solid principle.
+//select max(sal) from emp where sal<=(select max(sal) from emp);
+//
+//select sal from emp where denc_rank (0,2) order by sal;
+//
+////test the private method in unit test cases
+//@test
+//public void testPrivatemethod() throws Exception {
+//    Method method = MyClass.class.getDeclared;
+//    reflectMethod.setAccessible(true);
+//}
+//
+//Design patterns, Design principles, Solid principle.
